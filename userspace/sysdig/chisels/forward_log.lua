2a937ea198a8dac9289c3dc1e2b8a27859453d10
Sereyvathana Ty <serey789@gmail.com>
2014-04-05 13:30:05 -0600

    forward to a server over http

--- userspace/sysdig/chisels/forward_log.lua
+++ userspace/sysdig/chisels/forward_log.lua
@@ -1,6 +1,6 @@
 --[[
 Copyright (C) 2013-2014 Draios inc.
- 
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License version 2 as
 published by the Free Software Foundation.
@@ -20,10 +20,13 @@ description = "Send log to a server. Combine this script with a filter to limit 
 short_description = "Print stdout of processes";
 category = "I/O";
 
+-- package.path="./?.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua;/usr/local/lib/lua/5.1/?.lua;/usr/local/lib/lua/5.1/?/init.lua;/usr/share/lua/5.1/?.lua;/usr/share/lua/5.1/?/init.lua";
+package.path = "/usr/share/lua/5.1/?.lua;/usr/share/lua/5.1/?/init.lua;" .. package.path;
+package.cpath = "/usr/lib/i386-linux-gnu/lua/5.1/?.so;" .. package.cpath;
 args = {
     {
-        name = "server_name", 
-        description = "the name of the server that the log will be forward to", 
+        name = "server_name",
+        description = "the name of the server that the log will be forward to",
         argtype = "string"
     },
 }
@@ -38,33 +41,36 @@ end
 function on_init()
 	-- Request the fileds that we need
 	fbuf = chisel.request_field("evt.rawarg.data")
+    eventNumber =  chisel.request_field("evt.num");
+    eventTime =  chisel.request_field("evt.time");
+    procName = chisel.request_field("proc.name");
 
-	-- increase the snaplen so we capture more of the conversation 
+	-- increase the snaplen so we capture more of the conversation
 	sysdig.set_snaplen(2000)
-	
+
 	-- set the filter
-	chisel.set_filter("fd.num=1 and evt.is_io=true")
-	
+	chisel.set_filter("evt.is_io=true")
 	return true
 end
 
 -- Event parsing callback
 function on_event()
-	buf = evt.field(fbuf)
-	
+    buf = string.format("%s %s %s", evt.field(eventNumber), evt.field(eventTime), evt.field(procName));
+    local http = require("socket.http");
+    local request_body = string.format("event=%s", buf);
+    -- print(buf)
+    local response_body = { };
 	if buf ~= nil then
-        local request_body = string.format("[[event=%s]]", buf);
-		local http = require("socket.http");
-        b, c, h = http.request { 
+        b, c, h = http.request {
             url = server_url;
             method = "POST";
             headers = {
                 ["Content-Type"] = "application/x-www-form-urlencoded";
                 ["Content-Length"] = #request_body;
             };
+            source = ltn12.source.string(request_body);
+            sink = ltn12.sink.table(response_body);
         }
-        source = ltn12.source.string(request_body);
-        sink = ltn12.sink.table(response_body);
 	end
 	return true
 end
